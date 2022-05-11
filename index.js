2480deca0680c7868b545fc0e879274017cc6105
Alan Akra <alanakra@hotmail.fr>
2022-05-11 19:14:53 +0200

    Check if line 1 of the input is counted

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const inputForm = document.getElementById('input-form')
 const outputForm = document.getElementById('output-form')
 const inputValue = document.getElementById('input-csv').value
 const outputValue = document.getElementById('output-form')
-const firstLineOk = document.getElementById('firstLineOk')
-outputValue.value = ''
+let firstLineOk = document.getElementById('firstLineOk').checked
+
 
 inputForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const rows = inputValue.replace(/\n\r?/g, '\n').split('\n');
+    let rows = firstLineOk == true ? inputValue.replace(/\n\r?/g, '\n').split('\n') : inputValue.replace(/\n\r?/g, '\n').split('\n').slice(1)
     console.log(rows)
     outputValue.value = rows
 })
