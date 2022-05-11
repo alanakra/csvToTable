e79fee3ed60237cb8e3e55ffe0b5f5ac66ae6841
Alan Akra <alanakra@hotmail.fr>
2022-05-11 13:39:03 +0200

    Break line in textarea

--- index.js
+++ index.js
@@ -4,26 +4,10 @@ const inputValue = document.getElementById('input-csv').value
 const outputValue = document.getElementById('output-form')
 const firstLineOk = document.getElementById('firstLineOk')
 outputValue.value = ''
-const str = `Prénom,ville,Pharmacie,cadeau
-Patricia M.,GUJAN MESTRAS,PHARM DE MEYRAN,Robot Multifonction Silver KENWOOD
-Anaïs L.,CHATEAURENARD,PHARM DU COURS SELARL,Robot Multifonction Silver KENWOOD
-jean-luc L.,CHOUZELOT,PHARM VERDENET,Multi-Styler 10 en 1 PHILIPS
-Anais M.,montpellier,PHARM DU SOLEIL SELAS,Multi-Styler 10 en 1 PHILIPS
-Angélique S.,Roquefort-sur-Soulzon,PHARM DE LA POSTE,Multi-Styler 10 en 1 PHILIPS
-Mylène A.,Nice,GRANDE PHARM ANGLO AMERICAINE SELA,Multi-Styler 10 en 1 PHILIPS
-armelle L.,toulouse,PHARM TOLOSANE S N C,Multi-Styler 10 en 1 PHILIPS
-kevin t.,Mignaloux-Beauvoir ,PHARM DU GEANT CASINO,UV-AGE DAILY SPF50+
-Laetitia A.,Grandfontaine ,PHARM VERDENET,UV-AGE DAILY SPF50+
-`
-console.log(str.split('\n'))
-console.log(str.split('\n').slice(1))
 
 inputForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    //console.log(typeof(inputValue))
-    const rows = inputValue.split('\n')
-    //console.log(rows)
-
-
-    outputValue.value = inputValue
+    const rows = inputValue.replace(/\n\r?/g, '\n').split('\n');
+    console.log(rows)
+    outputValue.value = rows
 })
