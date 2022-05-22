ac09c666c214fa5fab706c384473767df9a23dc8
Alan Akra <alanakra@hotmail.fr>
2022-05-22 23:35:33 +0200

    Add inputs radio

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const outputForm = document.getElementById('output-form')
 const inputValue = document.getElementById('input-csv').value
 const outputValue = document.getElementById('output-form')
 let firstLineOk = document.getElementById('firstLineOk').checked
+const startString = `
+<table style="width: 960px;" width="960" cellspacing="0" cellpadding="0" border="0">
+<colgroup><col style="width: 240px;" width="240" /><col style="width: 240px;" width="240" /><col style="width: 240px;" width="240" /><col style="width: 240px;" width="240" /></colgroup>
+<tbody>
+<tr style="height: 16pt;" height="21">
+<td class="xl66" style="color: white; background: #ffc000; height: 16pt; width: 240px; border: none; text-align: left;" align="left">PR&Eacute;NOM</td>
+<td class="xl66" style="color: white; background: #ffc000; width: 240px; border: none; text-align: left;" align="left">VILLE</td>
+<td class="xl66" style="color: white; background: #ffc000; width: 240px; border: none; text-align: left;" align="left">PHARMACIE</td>
+</tr>
+`
+const endString = `
+</tbody>
+</table>
+<br />
+<br class="t-last-br" />
+`
 
 
 
