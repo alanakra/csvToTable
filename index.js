a54af0e874a38f415252f33ed38405b6af3b4763
Alan Akra <alanakra@hotmail.fr>
2022-05-23 22:04:12 +0200

    Remove align

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ inputForm.addEventListener('submit', (e) => {
             const arrPers = index.replace(/\n\r?/g, '\n').split('\n')[0].split(',')
             const template = (`
             <tr style="height: 16pt;" height="21">
-            	<td style="color: black; height: 16pt; width: 240px; border: none; text-align: left;" align="left"><span style="text-align: start; letter-spacing: normal;">${arrPers[0]}</span></td>
-            	<td class="xl65" style="color: black; border: none; text-align: left; width: 240px;" align="left"><span style="text-align: start; letter-spacing: normal;">${arrPers[1]}</span></td>
-            	<td class="xl65" style="color: black; border: none; text-align: left; width: 240px;" align="left"><span style="text-align: start; letter-spacing: normal;">${arrPers[2]}</span></td>
+            	<td style="color: black; height: 16pt; width: 240px; border: none; text-align: left;"><span style="text-align: start; letter-spacing: normal;">${arrPers[0]}</span></td>
+            	<td class="xl65" style="color: black; border: none; text-align: left; width: 240px;"><span style="text-align: start; letter-spacing: normal;">${arrPers[1]}</span></td>
+            	<td class="xl65" style="color: black; border: none; text-align: left; width: 240px;"><span style="text-align: start; letter-spacing: normal;">${arrPers[2]}</span></td>
         	</tr>
             `)
             console.log(template)
