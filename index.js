f1dbdaa9b463ce4f1084d766af9545c2ff4cc14a
Alan Akra <alanakra@hotmail.fr>
2022-06-09 01:09:52 +0200

    Retrieve the value of input on submit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inputForm = document.getElementById('input-form')
 const outputForm = document.getElementById('output-form')
-const inputValue = document.getElementById('input-csv').value
 const outputValue = document.getElementById('output-form')
 let firstLineOk = document.getElementById('firstLineOk').checked
 
@@ -8,6 +7,7 @@ let firstLineOk = document.getElementById('firstLineOk').checked
 
 inputForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    const inputValue = document.getElementById('input-csv').value
     let rows = firstLineOk == true ? inputValue.replace(/\n\r?/g, '\n').split('\n') : inputValue.replace(/\n\r?/g, '\n').split('\n').slice(1)
     console.log(rows)
     rows.forEach(
